5095f4961ea6ece7c91691489f485090a5f82ad9
Esben Sune Rasmussen <drphrozen@gmail.com>
2011-07-10 23:32:17 +0200

    initial commit

---
+++ rxtxTerminalRcp/src/dk/znz/rxtxterminalrcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package dk.znz.rxtxterminalrcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "dk.znz.rxtxterminalrcp.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
